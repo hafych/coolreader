d6905b2d2b33567d9f8cabed63463571a7d67fa3
Nazarii Hafych <nazariihafych@gmail.com>
2026-07-23 16:40:58 +0200

    ci: make clean checkouts reproducible

--- fb2props/fb2propstest.cpp
+++ fb2props/fb2propstest.cpp
@@ -26,17 +26,8 @@
 //prototype
 int GetBookProperties(char *name,  struct BookProperties* pBookProps, int localLanguage);
 
-#ifdef _DEBUG
-extern int STARTUP_FLAG;
-#endif
-
-
 int main( int argc, char * argv[] )
 {
-#ifdef _DEBUG
-    STARTUP_FLAG = 0;
-#endif
-
     //CRLog::setStdoutLogger();
     //CRLog::setLogLevel( CRLog::LL_TRACE );
     if ( argc!=2 ) {
@@ -65,8 +56,5 @@ int main( int argc, char * argv[] )
     printf("Book author: %s\n", props.author);
     printf("Book series: %s\n", props.series);
     //getchar();
-#ifdef _DEBUG
-    STARTUP_FLAG = 1;
-#endif
     return 0;
 }
